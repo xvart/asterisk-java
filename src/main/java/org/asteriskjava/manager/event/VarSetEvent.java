83f3df51c6fd24643b805fb04aad5c1bd534cd80
xvart <xvart@languid.me.uk>
2015-03-04 19:59:07 +1100

    changes for asterisk 13

--- src/main/java/org/asteriskjava/manager/event/VarSetEvent.java
+++ src/main/java/org/asteriskjava/manager/event/VarSetEvent.java
@@ -33,7 +33,17 @@ public class VarSetEvent extends ManagerEvent
     private String uniqueId;
     private String variable;
     private String value;
-
+    private String channelstatedesc;
+    private String calleridname;
+    private String language;
+    private String exten;
+    private String calleridnum;
+    private String context;
+    private String connectedlinenum;
+    private String connectedlinename;
+    private Integer priority;
+    private Integer channelstate;
+    
     public VarSetEvent(Object source)
     {
         super(source);
@@ -97,5 +107,85 @@ public class VarSetEvent extends ManagerEvent
     public void setValue(String value)
     {
         this.value = value;
+    }
+
+    public String getChannelstatedesc() {
+        return channelstatedesc;
+    }
+
+    public void setChannelstatedesc(String channelstatedesc) {
+        this.channelstatedesc = channelstatedesc;
+    }
+
+    public String getCalleridname() {
+        return calleridname;
+    }
+
+    public void setCalleridname(String calleridname) {
+        this.calleridname = calleridname;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getExten() {
+        return exten;
+    }
+
+    public void setExten(String exten) {
+        this.exten = exten;
+    }
+
+    public String getCalleridnum() {
+        return calleridnum;
+    }
+
+    public void setCalleridnum(String calleridnum) {
+        this.calleridnum = calleridnum;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+
+    public String getConnectedlinenum() {
+        return connectedlinenum;
+    }
+
+    public void setConnectedlinenum(String connectedlinenum) {
+        this.connectedlinenum = connectedlinenum;
+    }
+
+    public String getConnectedlinename() {
+        return connectedlinename;
+    }
+
+    public void setConnectedlinename(String connectedlinename) {
+        this.connectedlinename = connectedlinename;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Integer getChannelstate() {
+        return channelstate;
+    }
+
+    public void setChannelstate(Integer channelstate) {
+        this.channelstate = channelstate;
     }
 }
